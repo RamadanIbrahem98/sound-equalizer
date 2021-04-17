9f67ab51ae35a209dcb4c8887cffcda2f02b2404
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-04-17 23:08:16 +0200

    add icons and PDF class

---
+++ PDF.py
@@ -0,0 +1,70 @@
+from fpdf import FPDF
+import os
+import re
+
+from scipy.signal.spectral import spectrogram
+
+
+
+
+class PDF(FPDF):
+    def __init__(self):
+        super().__init__()
+        self.WIDTH = 210
+        self.HEIGHT = 297
+
+
+
+    def header(self):
+        # Custom logo and positioning
+        # Create an `assets` folder and put any wide and short image inside
+        # Name the image `logo.png`
+        # 10 distance from left   8 distance from top   33 size
+        self.image('.\\assets/logo.png', 10, 8, 35)
+        self.image('.\\assets/CUFE.png', 170, 6, 25)
+
+        self.set_font('helvetica', 'B', 15)
+        self.cell(self.WIDTH - 142)
+        self.cell(60, 1, 'Sound Equalizer', 0, 0, 'C')
+        self.ln(20)
+#
+
+    def footer(self):
+        # Page numbers in the footer
+        self.set_y(-15)
+        self.set_font('helvetica', 'I', 8)
+        self.set_text_color(128)
+        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
+
+    def page_body(self, images, PLOT_DIR):
+        # image    15 from left   25 from top    self.width - 30 -> distance to right (width of the graph)
+        self.image(PLOT_DIR + '/' + images[0], 15, 30, self.WIDTH - 30)
+        self.image(PLOT_DIR + '/' +
+                   images[1], 15, self.WIDTH / 2 + 20, self.WIDTH - 30)
+            
+        # self.cell('Dr.Name: %d' %data[0])
+        # self.ln(190)
+        # self.cell(0,3,f'Dr.Name: {data[0][0]}                    Speciality: {data[0][1]}')
+        # self.ln(7)
+        # self.cell(0,3,f'Patient Name: {data[0][2]}                    Age: {data[0][3]}')
+        # self.ln(7)
+        # self.cell(0,3,f'Diagnosis: {data[0][4]}    Medicine: {data[0][5]}   Dose: {data[0][6]} /day  ')            
+        #self.cell(0.3,f'Speciality: {data[0][0]}')
+        
+
+    def print_page(self, images, PLOT_DIR):
+        # Generates the report
+        self.add_page()
+        self.page_body(images, PLOT_DIR)
+
+    def construct(self, PLOT_DIR):
+        pages_data = []
+        # Get all plots
+        files = os.listdir(PLOT_DIR)
+        # Sort them by month - a bit tricky because the file names are strings
+        files = sorted(os.listdir(PLOT_DIR),
+                       key=lambda x: x.split('.')[0])
+        pages = len(files) // 2
+        for i in range(pages):
+            pages_data.append([files[0+i], files[pages+i]])
+        return [*pages_data]
